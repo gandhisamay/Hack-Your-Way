fb6f4e2f9737827dbb510eac4cb705864f3ba266
Labeeb02 <ahsan.labeeb@gmail.com>
2022-11-12 14:12:48 +0530

    img to txt

---
+++ image_to_txt/main.py
@@ -0,0 +1,35 @@
+import io
+import os
+# Imports the Google Cloud client library
+from google.cloud import vision
+
+# Instantiates a client
+client = vision.ImageAnnotatorClient()
+
+# The name of the image file to annotate
+file_name = os.path.abspath('pic.png')
+
+
+with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
+
+
+image = vision.Image(content=content)
+
+response = client.text_detection(image=image)
+texts = response.text_annotations
+print('Texts:')
+
+for text in texts:
+    print('\n"{}"'.format(text.description))
+
+    vertices = (['({},{})'.format(vertex.x, vertex.y)
+                for vertex in text.bounding_poly.vertices])
+
+    print('bounds: {}'.format(','.join(vertices)))
+
+if response.error.message:
+    raise Exception(
+        '{}\nFor more info on error messages, check: '
+        'https://cloud.google.com/apis/design/errors'.format(
+            response.error.message))
