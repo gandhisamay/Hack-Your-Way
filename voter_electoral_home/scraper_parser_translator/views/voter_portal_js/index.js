267d42d297d090f5d150ffa689eaf031d7cc4a04
Samay Gandhi <gandhisamay200@gmail.com>
2022-11-16 11:01:15 +0530

    voter portal js

---
+++ voter_electoral_home/scraper_parser_translator/views/voter_portal_js/index.js
@@ -0,0 +1,31 @@
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+let func = async function() {
+  url = "https://electoralsearch.in/Home/searchVoter"
+  data = {
+    "txtCaptcha": "qIyuDe",
+    "search_type": "details",
+    "reureureired": "ca3ac2c8-4676-48eb-9129-4cdce3adf6ea",
+    "name": "Nirali Gandhi",
+    "rln_name": "Amit Gandhi",
+    "page_no": 1,
+    "location": "S13,,",
+    "results_per_page": 10,
+    "location_range": "20",
+    "age": 45,
+    "dob": null,
+    "gender": "F"
+  }
+
+  let res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data)
+  })
+
+  let result = await res.text()
+  console.log(result)
+}
+
+func()
